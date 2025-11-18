cdec1a7e54d655a776cf55c8fd83c9e01b784791
peipei <peipei@qq.com>
2025-11-18 19:41:01 +0800

    ObDropdownMenu

--- app/src/main/java/cn/coolbet/orbit/ui/kit/ListTile.kt
+++ app/src/main/java/cn/coolbet/orbit/ui/kit/ListTile.kt
@@ -1,65 +1,35 @@
 package cn.coolbet.orbit.ui.kit
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.SubcomposeLayout
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
 import cn.coolbet.orbit.R
 import cn.coolbet.orbit.ui.theme.AppTypography
-import cn.coolbet.orbit.ui.theme.Black25
-import cn.coolbet.orbit.ui.theme.Black50
 import cn.coolbet.orbit.ui.theme.ObTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
 
 //headlineContent  title  主标题
 //overlineContent 上标题
@@ -98,176 +68,6 @@ fun MeasureView() {
         // 由于 SubcomposeLayout 在这里只用于测量，我们返回一个空的布局
         layout(0, 0) {}
     }
-}
-
-@SuppressLint("RememberReturnType")
-@Composable
-fun ListTileChevronUpDown(
-    title: String, trailing: String, icon: Int,
-    menuContent: @Composable () -> Unit = {}
-) {
-    var expanded by remember { mutableStateOf(false) }
-    var anchorSize by remember { mutableStateOf(IntSize.Zero) }
-    var menuContentWidth by remember { mutableIntStateOf(240) }
-    val interactionSource = remember { MutableInteractionSource() }
-    val scope = rememberCoroutineScope()
-    var isClickDisabled by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        // 监听 showPopup 状态流
-        snapshotFlow { expanded }
-            // 过滤：我们只关心状态从 true 变为 false (即弹窗被关闭) 的情况
-            .filter { !it }
-            .collect {
-                // 当 showPopup 刚刚变为 false 时：
-
-                // 1. 立即禁用 Row 的点击
-                isClickDisabled = true
-
-                // 2. 引入一个短暂延迟，以确保 Row 的 clickable 在状态改变后立即执行时被忽略
-                delay(100)
-
-                // 3. 延迟结束后，解除点击禁用
-                isClickDisabled = false
-            }
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(48.dp)
-            .background(ObTheme.colors.primaryContainer)
-            .clickable(
-                enabled = !isClickDisabled,
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null,
-            ){
-                expanded = !expanded
-            },
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Spacer(modifier = Modifier.width(16.dp))
-        Image(
-            modifier = Modifier.size(24.dp),
-            painter = painterResource(id = icon),
-            contentDescription = "",
-            contentScale = ContentScale.Fit,
-            colorFilter = ColorFilter.tint(ObTheme.colors.secondary),
-        )
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(
-            title,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            style = AppTypography.R15,
-            modifier = Modifier.weight(1f)
-        )
-        Spacer(modifier = Modifier.width(16.dp)) // 12 + 4
-
-        // --- 关键区域：局部 Box 锚点 ---
-        Box(
-            // 关键：在这里测量 Box 的尺寸
-            modifier = Modifier
-                .fillMaxHeight()
-                .wrapContentSize(Alignment.TopEnd)
-                .onSizeChanged { anchorSize = it } // 记录锚点 Box 的实际尺寸
-        ) {
-            // 1. 锚点内容（右侧的 Text 和 Image）
-            Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
-                Text(trailing, maxLines = 1, overflow = TextOverflow.Ellipsis, style = AppTypography.R15B50)
-                Spacer(modifier = Modifier.width(4.dp)) //4 + 4
-                Image(
-                    modifier = Modifier.size(20.dp),
-                    painter = painterResource(id = R.drawable.chevron_up_down),
-                    contentDescription = "",
-                    contentScale = ContentScale.Inside,
-                    colorFilter = ColorFilter.tint(ObTheme.colors.tertiary),
-                )
-                Spacer(modifier = Modifier.width(12.dp))
-            }
-
-            CustomDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-            ) {
-                Column {
-                    menuContent()
-                }
-            }
-        }
-    }
-
-//    Row(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .height(48.dp)
-//            .background(ObTheme.colors.primaryContainer)
-//            .clickable(
-//                indication = null,
-//                interactionSource = interactionSource,
-//                onClick = { expanded = !expanded }
-//            ),
-//        verticalAlignment = Alignment.CenterVertically,
-//    ) {
-//        Spacer(modifier = Modifier.width(16.dp))
-//        Image(
-//            modifier = Modifier.size(24.dp),
-//            painter = painterResource(id = icon),
-//            contentDescription = "",
-//            contentScale = ContentScale.Fit,
-//            colorFilter = ColorFilter.tint(ObTheme.colors.secondary),
-//        )
-//        Spacer(modifier = Modifier.width(12.dp))
-//        Text(
-//            title,
-//            maxLines = 1,
-//            overflow = TextOverflow.Ellipsis,
-//            style = AppTypography.R15,
-//            modifier = Modifier.weight(1f)
-//        )
-//        Spacer(modifier = Modifier.width(16.dp)) // 12 + 4
-//
-//        // --- 关键区域：局部 Box 锚点 ---
-//        Box(
-//            // 关键：在这里测量 Box 的尺寸
-//            modifier = Modifier
-//                .fillMaxHeight()
-//                .wrapContentSize(Alignment.TopEnd)
-//                .onSizeChanged { anchorSize = it } // 记录锚点 Box 的实际尺寸
-//        ) {
-//            // 1. 锚点内容（右侧的 Text 和 Image）
-//            Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
-//                Text(trailing, maxLines = 1, overflow = TextOverflow.Ellipsis, style = AppTypography.R15B50)
-//                Spacer(modifier = Modifier.width(4.dp)) //4 + 4
-//                Image(
-//                    modifier = Modifier.size(20.dp),
-//                    painter = painterResource(id = R.drawable.chevron_up_down),
-//                    contentDescription = "",
-//                    contentScale = ContentScale.Inside,
-//                    colorFilter = ColorFilter.tint(ObTheme.colors.tertiary),
-//                )
-//            }
-//
-//            // 2. DropdownMenu 锚定到此 Box
-//            DropdownMenu(
-//                containerColor = ObTheme.colors.primaryContainer,
-//                tonalElevation = 0.dp,//阴影
-//                shape = RoundedCornerShape(16.dp),
-//                expanded = expanded,
-//                onDismissRequest = { expanded = false },
-////                modifier = Modifier.padding(vertical = (-4).dp)
-////                    .offset { // 响应式偏移计算
-////                    // X_offset = 锚点 Box 宽度 - 菜单实际宽度 (测量值)
-////                    // 这个计算确保菜单的右边缘对齐到锚点 Box 的右边缘
-////                    val horizontalOffset = anchorSize.width - menuContentWidth
-////                    IntOffset(x = horizontalOffset, y = 0)
-////                }
-//            ) {
-//                menuContent()
-//            }
-//        }
-//        Spacer(modifier = Modifier.width(12.dp))
-//    }
 }
 
 
---
+++ app/src/main/java/cn/coolbet/orbit/ui/kit/ListTileChevronUpDown.kt
@@ -0,0 +1,121 @@
+package cn.coolbet.orbit.ui.kit
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import cn.coolbet.orbit.R
+import cn.coolbet.orbit.ui.theme.AppTypography
+import cn.coolbet.orbit.ui.theme.ObTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun ListTileChevronUpDown(
+    title: String, trailing: String, icon: Int,
+    menuContent: @Composable () -> Unit = {}
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var isClickDisabled by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        snapshotFlow { expanded }
+            .filter { !it }
+            .collect {
+                isClickDisabled = true
+                delay(100) // 2. 引入一个短暂延迟，以确保 Row 的 clickable 在状态改变后立即执行时被忽略
+                isClickDisabled = false
+            }
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .background(ObTheme.colors.primaryContainer)
+            .clickable(
+                enabled = !isClickDisabled,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+            ){
+                expanded = !expanded
+            },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Spacer(modifier = Modifier.width(16.dp))
+        Image(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = icon),
+            contentDescription = "",
+            contentScale = ContentScale.Fit,
+            colorFilter = ColorFilter.tint(ObTheme.colors.secondary),
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = AppTypography.R15,
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.width(16.dp)) // 12 + 4
+
+        // --- 关键区域：局部 Box 锚点 ---
+        Box(
+            // 关键：在这里测量 Box 的尺寸
+            modifier = Modifier
+                .fillMaxHeight()
+                .wrapContentSize(Alignment.TopEnd)
+        ) {
+            // 1. 锚点内容（右侧的 Text 和 Image）
+            Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
+                Text(trailing, maxLines = 1, overflow = TextOverflow.Ellipsis, style = AppTypography.R15B50)
+                Spacer(modifier = Modifier.width(4.dp)) //4 + 4
+                Image(
+                    modifier = Modifier.size(20.dp),
+                    painter = painterResource(id = R.drawable.chevron_up_down),
+                    contentDescription = "",
+                    contentScale = ContentScale.Inside,
+                    colorFilter = ColorFilter.tint(ObTheme.colors.tertiary),
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+            }
+
+            ObDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                Column {
+                    menuContent()
+                }
+            }
+        }
+    }
+}
