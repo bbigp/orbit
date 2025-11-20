0d99b3de745cbcec1df4c84781485fe35eea3a2c
peipei <peipei@qq.com>
2025-11-20 09:00:49 +0800

    folder radio

---
+++ app/src/main/java/cn/coolbet/orbit/ui/kit/ObRadio.kt
@@ -0,0 +1,92 @@
+package cn.coolbet.orbit.ui.kit
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import cn.coolbet.orbit.R
+
+@Composable
+fun ObRadio(
+    selected: Boolean = false,
+    onClick: () -> Unit = {},
+    sizes: ObRadioSizes = ObRadioDefaults.medium,
+) {
+    val haptics = LocalHapticFeedback.current
+    Box(
+        modifier = Modifier.size(sizes.size)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                onClick()
+                haptics.performHapticFeedback(HapticFeedbackType.ToggleOn)
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            modifier = Modifier.size(sizes.dotSize),
+            painter = painterResource(id = if (selected) R.drawable.selection_radio else R.drawable.selection_off),
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+        )
+    }
+}
+
+object ObRadioDefaults {
+    val small: ObRadioSizes = ObRadioSizes(size = 24.dp, dotSize = 15.dp)
+    val medium: ObRadioSizes = ObRadioSizes(size = 28.dp, dotSize = 21.dp)
+}
+
+data class ObRadioSizes(
+    val size: Dp,
+    val dotSize: Dp,
+)
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRadio() {
+    Column {
+        Row {
+            ObRadio(selected = true)
+            ObRadio(selected = false)
+        }
+        Row {
+            ObRadio(selected = true, sizes = ObRadioDefaults.small)
+        }
+
+        RadioButton(
+            selected = true,
+            onClick = {}
+        )
+        RadioButton(
+            selected = false,
+            onClick = {}
+        )
+        Checkbox(
+            checked = true,
+            onCheckedChange = {}
+        )
+        Checkbox(
+            checked = false,
+            onCheckedChange = {}
+        )
+    }
+}
--- app/src/main/java/cn/coolbet/orbit/ui/view/folder/FoldersCard.kt
+++ app/src/main/java/cn/coolbet/orbit/ui/view/folder/FoldersCard.kt
@@ -1,14 +1,20 @@
 package cn.coolbet.orbit.ui.view.folder
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -18,12 +24,17 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import cn.coolbet.orbit.R
 import cn.coolbet.orbit.model.domain.Feed
+import cn.coolbet.orbit.model.domain.Folder
 import cn.coolbet.orbit.ui.kit.ObCard
+import cn.coolbet.orbit.ui.kit.ObRadio
+import cn.coolbet.orbit.ui.kit.ObRadioDefaults
+import cn.coolbet.orbit.ui.theme.AppTypography
 
 
 @Composable
@@ -39,70 +50,45 @@ fun FoldersCard(
 
 
 @Composable
-fun ObRadio(
-    selected: Boolean = false,
-    onClick: () -> Unit = {},
-    sizes: ObRadioSizes = ObRadioDefaults.medium,
+fun FolderRadio(
+    folder: Folder,
+    selectedValue: Long = 0,
+    onValueChange: (Long) -> Unit = {}
 ) {
-    val haptics = LocalHapticFeedback.current
-    Box(
-        modifier = Modifier.size(sizes.size)
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null
-            ) {
-                onClick()
-                haptics.performHapticFeedback(HapticFeedbackType.ToggleOn)
-            },
-        contentAlignment = Alignment.Center,
+    Row(
+        modifier = Modifier.height(40.dp).fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
+        Spacer(modifier = Modifier.width(12.dp))
         Image(
-            modifier = Modifier.size(sizes.dotSize),
-            painter = painterResource(id = if (selected) R.drawable.selection_radio else R.drawable.selection_off),
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = R.drawable.group),
             contentDescription = "",
-            contentScale = ContentScale.FillBounds,
+            contentScale = ContentScale.Fit,
         )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            folder.title,
+            modifier = Modifier.weight(1f),
+            maxLines = 1, overflow = TextOverflow.Ellipsis,
+            style = AppTypography.R15
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        ObRadio(
+            selected = selectedValue == folder.id,
+            sizes = ObRadioDefaults.medium,
+            onClick = { onValueChange(folder.id) }
+        )
+        Spacer(modifier = Modifier.width(12.dp))
     }
 }
 
-object ObRadioDefaults {
-    val small: ObRadioSizes = ObRadioSizes(size = 24.dp, dotSize = 15.dp)
-    val medium: ObRadioSizes = ObRadioSizes(size = 28.dp, dotSize = 21.dp)
-}
-
-data class ObRadioSizes(
-    val size: Dp,
-    val dotSize: Dp,
-)
-
-
 @Preview(showBackground = true)
 @Composable
-fun PreviewRadio() {
-    Column {
-        Row {
-            ObRadio(selected = true)
-            ObRadio(selected = false)
-        }
-        Row {
-            ObRadio(selected = true, sizes = ObRadioDefaults.small)
-        }
+fun PreviewFolderRadio() {
 
-        RadioButton(
-            selected = true,
-            onClick = {}
-        )
-        RadioButton(
-            selected = false,
-            onClick = {}
-        )
-        Checkbox(
-            checked = true,
-            onCheckedChange = {}
-        )
-        Checkbox(
-            checked = false,
-            onCheckedChange = {}
-        )
+    Column {
+        FolderRadio(Folder.EMPTY)
+        FolderRadio(Folder.EMPTY, selectedValue = 1)
     }
 }
