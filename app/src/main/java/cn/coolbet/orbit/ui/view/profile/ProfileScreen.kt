5c5cb987f471d2617a7612d639d8c2b879fd1b16
peipei <peipei@qq.com>
2026-01-12 21:15:34 +0800

    profile change env

--- app/src/main/java/cn/coolbet/orbit/ui/view/profile/ProfileScreen.kt
+++ app/src/main/java/cn/coolbet/orbit/ui/view/profile/ProfileScreen.kt
@@ -1,52 +1,29 @@
 package cn.coolbet.orbit.ui.view.profile
 
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getScreenModel
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.currentOrThrow
 import cn.coolbet.orbit.R
+import cn.coolbet.orbit.manager.Env
+import cn.coolbet.orbit.manager.asOpenContentState
+import cn.coolbet.orbit.manager.asUnreadMarkState
 import cn.coolbet.orbit.model.domain.OpenContentWith
 import cn.coolbet.orbit.model.domain.UnreadMark
-import cn.coolbet.orbit.ui.kit.DragHandle
 import cn.coolbet.orbit.ui.kit.DropdownMenuDivider
 import cn.coolbet.orbit.ui.kit.ListTileChevronUpDown
 import cn.coolbet.orbit.ui.kit.ListTileSwitch
@@ -54,18 +31,11 @@ import cn.coolbet.orbit.ui.kit.OButtonDefaults
 import cn.coolbet.orbit.ui.kit.ObBackTopAppBar
 import cn.coolbet.orbit.ui.kit.ObCard
 import cn.coolbet.orbit.ui.kit.ObDropdownMenuItem
-import cn.coolbet.orbit.ui.kit.ObIcon
 import cn.coolbet.orbit.ui.kit.ObTextButton
-import cn.coolbet.orbit.ui.kit.ObTopAppbar
 import cn.coolbet.orbit.ui.kit.SpacerDivider
 import cn.coolbet.orbit.ui.kit.SystemBarStyleModern
-import cn.coolbet.orbit.ui.theme.AppTypography
-import cn.coolbet.orbit.ui.theme.ContainerSecondary
 import cn.coolbet.orbit.ui.theme.ObTheme
-import cn.coolbet.orbit.ui.view.folder.FolderPicker
 import cn.coolbet.orbit.ui.view.folder.FolderPickerSheet
-import cn.coolbet.orbit.ui.view.folder.PreviewFolderRadio
-import kotlinx.coroutines.launch
 
 object ProfileScreen: Screen {
     private fun readResolve(): Any = ProfileScreen
@@ -76,6 +46,10 @@ object ProfileScreen: Screen {
         val model = getScreenModel<ProfileScreenModel>()
         val state by model.state.collectAsState()
         val folders by model.folders.collectAsState()
+        val unreadMark by Env.settings.unreadMark.asUnreadMarkState()
+        val openContentWith by Env.settings.openContentWith.asOpenContentState()
+        val autoRead by Env.settings.autoRead.asState()
+        val rootFolderId by Env.settings.rootFolder.asState()
 
         var showFolderPicker by remember { mutableStateOf(false) }
         FolderPickerSheet (
@@ -84,9 +58,11 @@ object ProfileScreen: Screen {
                 showFolderPicker = false
             },
             folders = folders,
-            selectedValue = state.user.rootFolder,
+            selectedValue = rootFolderId,
             onValueChange = { id ->
-                model.changeUser(rootFolderId = id)
+                Env.settings.rootFolder.value = id
+                model.refreshRootFolder()
+                showFolderPicker = false
             }
         )
 
@@ -94,9 +70,9 @@ object ProfileScreen: Screen {
             UnreadMark.entries.forEachIndexed { index, mark ->
                 ObDropdownMenuItem(
                     text = mark.value, trailingIcon = mark.trailingIconRes,
-                    leadingIcon = if (state.user.unreadMark == mark) R.drawable.check else null,
+                    leadingIcon = if (unreadMark == mark) R.drawable.check else null,
                     onClick = {
-                        model.changeUser(unreadMark = mark)
+                        Env.settings.unreadMark.value = mark.value
                         onClose()
                     }
                 )
@@ -110,9 +86,9 @@ object ProfileScreen: Screen {
             OpenContentWith.entries.forEachIndexed { index, with ->
                 ObDropdownMenuItem(
                     text = with.value,
-                    leadingIcon = if (state.user.openContent == with) R.drawable.check else null,
+                    leadingIcon = if (openContentWith == with) R.drawable.check else null,
                     onClick = {
-                        model.changeUser(openContent = with)
+                        Env.settings.openContentWith.value = with.value
                         onClose()
                     }
                 )
@@ -128,7 +104,7 @@ object ProfileScreen: Screen {
             topBar = {
                 ObBackTopAppBar(
                     background = ObTheme.colors.secondaryContainer,
-                    title = "设置",
+//                    title = "设置",
                 )
             }
         ) { paddingValues ->
@@ -143,21 +119,21 @@ object ProfileScreen: Screen {
                     ObCard {
                         ListTileChevronUpDown(
                             title = "未读标记", icon = R.drawable.unread_dashed,
-                            trailing = state.user.unreadMark.value,
+                            trailing = unreadMark.value,
                             menuContent = unreadMarkMenus
                         )
                         SpacerDivider(start = 52.dp, end = 12.dp)
                         ListTileChevronUpDown(
                             title = "默认打开方式", icon = R.drawable.page,
-                            trailing = state.user.openContent.value,
+                            trailing = openContentWith.value,
                             menuContent = openContentMenus
                         )
                         SpacerDivider(start = 52.dp, end = 12.dp)
                         ListTileSwitch(
                             title = "自动已读", icon = R.drawable.check_o,
-                            checked = state.user.autoRead,
+                            checked = autoRead,
                             onCheckedChange = { v->
-                                model.changeUser(autoRead = v)
+                                Env.settings.autoRead.value = v
                             }
                         )
                         SpacerDivider(start = 52.dp, end = 12.dp)
@@ -166,14 +142,6 @@ object ProfileScreen: Screen {
                             trailing = state.rootFolder.title,
                             onClick = {
                                 showFolderPicker = true
-                            }
-                        )
-                        SpacerDivider(start = 52.dp, end = 12.dp)
-                        ListTileSwitch(
-                            title = "Automatic Reader View", icon = R.drawable.book,
-                            checked = state.user.autoReaderView,
-                            onCheckedChange = { v->
-                                model.changeUser(autoReaderView = v)
                             }
                         )
                     }
--- app/src/main/java/cn/coolbet/orbit/ui/view/profile/ProfileScreenModel.kt
+++ app/src/main/java/cn/coolbet/orbit/ui/view/profile/ProfileScreenModel.kt
@@ -5,23 +5,16 @@ import cafe.adriel.voyager.core.model.screenModelScope
 import cn.coolbet.orbit.manager.CacheStore
 import cn.coolbet.orbit.NavigatorBus
 import cn.coolbet.orbit.Route
-import cn.coolbet.orbit.manager.Preference
+import cn.coolbet.orbit.manager.Env
 import cn.coolbet.orbit.manager.Session
 import cn.coolbet.orbit.model.domain.Folder
-import cn.coolbet.orbit.model.domain.OpenContentWith
-import cn.coolbet.orbit.model.domain.UnreadMark
 import cn.coolbet.orbit.model.domain.User
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,7 +23,6 @@ import javax.inject.Inject
 class ProfileScreenModel @Inject constructor(
     private val store: CacheStore,
     private val session: Session,
-    private val preference: Preference,
 ): ScreenModel {
 
     private val _state = MutableStateFlow(ProfileState())
@@ -39,19 +31,13 @@ class ProfileScreenModel @Inject constructor(
     val folders = store.foldersState
 
     init {
-        _state.update { it.copy(isLoading = true) }
         session.state
             .filter { it.isNotEmpty }
-            .flatMapLatest { user ->
-                store.flowFolder(user.rootFolder)
-                    .map { folder ->
-                        folder to user
-                    }
-            }
-            .onEach { (folder, user) ->
-                _state.update { it.copy(isLoading = false, user = user, rootFolder = folder) }
+            .map { user ->
+                _state.update { it.copy(user = user) }
             }
             .launchIn(screenModelScope)
+        refreshRootFolder()
     }
 
     fun logout() {
@@ -59,15 +45,10 @@ class ProfileScreenModel @Inject constructor(
         NavigatorBus.replaceAll(Route.Login)
     }
 
-    fun changeUser(unreadMark: UnreadMark? = null, autoRead: Boolean? = null,
-                   openContent: OpenContentWith? = null, rootFolderId: Long? = null,
-                   autoReaderView: Boolean? = null,
-    ) {
-        val newUser = preference.userSetting(unreadMark = unreadMark, autoRead = autoRead,
-            openContent = openContent, rootFolderId = rootFolderId,
-            autoReaderView = autoReaderView,
-        )
-        session.startSession(newUser)
+    fun refreshRootFolder() {
+        _state.update {
+            it.copy(rootFolder = store.folder(Env.settings.rootFolder.value))
+        }
     }
 
     fun deleteLocalData() {
